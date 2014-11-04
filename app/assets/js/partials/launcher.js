e240a7e03779d3ae229354e22a21795d02b5d471
Colin Rotherham <work@colinr.com>
2014-11-04 13:37:48 +0000

    Update to Universal Analytics

--- app/assets/js/partials/launcher.js
+++ app/assets/js/partials/launcher.js
@@ -113,17 +113,10 @@
 	Google Analytics
 	----------------------------------- */
 
-	// set up account
-	var _gaq = [['_setAccount', 'UA-2204911-2']];
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	// track page view
-	_gaq.push(['_trackPageview']);
-
-	(function()
-	{
-		var ga = document.createElement('script');
-		ga.async = true; ga.src = "//www.google-analytics.com/ga.js";
-
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	})();
+	ga('create', 'UA-2204911-2', 'auto');
+	ga('send', 'pageview');
