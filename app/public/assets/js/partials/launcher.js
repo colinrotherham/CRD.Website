2e81456c7f709479ccb6e27db7bc273b56de31b7
Colin Rotherham <work@colinr.com>
2015-05-06 22:58:25 +0100

    Use head.js to inject Google Analytics
    Relies on NginX proxy location:
    /assets/js/analytics

--- app/public/assets/js/partials/launcher.js
+++ app/public/assets/js/partials/launcher.js
@@ -106,17 +106,11 @@
 			new namespace.EmailUnscramble(email, [101, 211, 223, 230, 222, 219, 219, 206, 216, 179, 163, 210, 219, 213, 215, 224, 160, 145, 210, 220]);
 		});
 
+		// Inject Google Analytics
+		head.js('/assets/js/analytics', function()
+		{
+			window.ga('create', 'UA-2204911-2', 'auto');
+			window.ga('send', 'pageview', { transport: 'beacon' });
+		});
+
 	})(CRD.website);
-
-
-/*
-	Google Analytics
-	----------------------------------- */
-
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-2204911-2', 'auto');
-	ga('send', 'pageview');
